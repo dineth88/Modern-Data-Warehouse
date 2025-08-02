b94081ef985cde914ebe6b457b13849b009da8e8
dineth88 <dinethoshitha00@gmail.com>
2025-08-02 10:58:18 +0530

    Altered the truncating tables names in ddl scripts

--- Scripts/bronze/ddl_bronze.sql
+++ Scripts/bronze/ddl_bronze.sql
@@ -13,8 +13,8 @@ CREATE TABLE bronze.crm_cust_info (
 
 GO
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info (
     prd_id       INT,
@@ -28,8 +28,8 @@ CREATE TABLE bronze.crm_prd_info (
 
 GO
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
@@ -45,8 +45,8 @@ CREATE TABLE bronze.crm_sales_details (
 
 GO
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
 
 CREATE TABLE bronze.erp_loc_a101 (
     cid    NVARCHAR(50),
@@ -55,8 +55,8 @@ CREATE TABLE bronze.erp_loc_a101 (
 
 GO
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
 
 CREATE TABLE bronze.erp_cust_az12 (
     cid    NVARCHAR(50),
@@ -66,8 +66,8 @@ CREATE TABLE bronze.erp_cust_az12 (
 
 GO
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
--- Scripts/silver/ddl_silver.sql
+++ Scripts/silver/ddl_silver.sql
@@ -14,8 +14,8 @@ CREATE TABLE silver.crm_cust_info (
 
 GO
 
-IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
@@ -30,8 +30,8 @@ CREATE TABLE silver.crm_prd_info (
 
 GO
 
-IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
 
 CREATE TABLE silver.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
@@ -48,8 +48,8 @@ CREATE TABLE silver.crm_sales_details (
 
 GO
 
-IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
 
 CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
@@ -59,8 +59,8 @@ CREATE TABLE silver.erp_loc_a101 (
 
 GO
 
-IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
 
 CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
@@ -71,8 +71,8 @@ CREATE TABLE silver.erp_cust_az12 (
 
 GO
 
-IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_cust_info;
+IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
