078134e29e75e29f437732d0a5168e1bed0344dc
dineth88 <dinethoshitha00@gmail.com>
2025-08-10 19:32:04 +0530

    Added descriptions for crm_sales_info transformatrions

--- Scripts/silver/transformations.sql
+++ Scripts/silver/transformations.sql
@@ -94,11 +94,11 @@ END AS sls_due_dt,
 CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
 		THEN sls_quantity * ABS(sls_price)
 	ELSE sls_sales
-END AS sls_sales,
+END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
 sls_quantity,
 CASE WHEN sls_price IS NULL OR sls_price <= 0
 		THEN sls_sales / NULLIF(sls_quantity, 0)
 	ELSE sls_price
-END AS sls_price
+END AS sls_price -- Derive price if original value is invalid
 FROM bronze.crm_sales_details
 
