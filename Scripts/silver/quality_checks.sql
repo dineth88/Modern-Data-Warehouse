9f066f2568e5039674a1d7752d70a5bfedfea7ed
dineth88 <dinethoshitha00@gmail.com>
2025-08-10 13:01:26 +0530

    Added crm_sales_details table qualility cheching scripts

--- Scripts/silver/quality_checks.sql
+++ Scripts/silver/quality_checks.sql
@@ -49,3 +49,35 @@ SELECT sls_prd_key FROM bronze.crm_sales_details
 SELECT * 
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt
+
+-- ======================== sls_order_dt ============================
+
+select 
+NULLIF(sls_order_dt, 0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0
+
+--check for invalid dates
+select 
+NULLIF(sls_order_dt, 0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR LEN(sls_order_dt) != 8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
+
+select * 
+from bronze.crm_sales_details 
+where sls_order_dt > sls_ship_dt AND sls_order_dt > sls_due_dt
+
+-- Check data consistency between Sales, Quantity and Price
+-- >> Sales = Qunatity * Price
+-- >> Values must be NOT NULL, zer or negative
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
